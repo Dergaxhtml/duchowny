31c54eaf17edbc79b2867db5548b73dcb9c4496a
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-08-28 22:16:26 +0200

    Initial commit

---
+++ src/main/java/com/pdt/duchowny/DuchownyApplication.java
@@ -0,0 +1,13 @@
+package com.pdt.duchowny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuchownyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuchownyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdt/duchowny/DuchownyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdt.duchowny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuchownyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
